a5088cf1724adbf2d272b52a44d73dca337a7c23
GauravJangra9988 <gjangra9988@gmail.com>
2025-08-27 21:08:06 +0530

    updated cors

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:8081"},
+		AllowOrigins:     []string{"http://localhost:8081", "http://localhost:8081", "http://localhost:8080,https://clipnote-frontend.vercel.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept"},
 		ExposeHeaders:    []string{"Content-Length", "Authorization"},

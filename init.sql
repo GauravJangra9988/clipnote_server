707c4dbc9f8ac55212666b161f6fc45356dc3332
GauravJangra9988 <gjangra9988@gmail.com>
2025-11-16 00:55:07 +0530

    fixed db query

--- init.sql
+++ init.sql
@@ -1,14 +1,14 @@
 CREATE TABLE users (
     user_name varchar(100),
     email varchar(100),
-    password varchar(100),
-)
+    password varchar(100)
+);
 
 CREATE TABLE clip_data (
-    id primary serial,
+    id serial primary key,
     user_name varchar(100),
     title varchar,
     text text,
     tag varchar,
-    added_at timestamp default now(),
-)
+    added_at timestamp default now()
+);
